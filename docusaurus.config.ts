f60c16e479d8d51a17ff9286d39fae13aa8904d8
zenheart <zenheart_register@163.com>
2025-01-05 21:34:10 +0800

    fix: add webinterview

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -89,7 +89,7 @@ const config: Config = {
           ]
         }
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`
+      copyright: `Copyright © ${new Date().getFullYear()} Web Interview, Inc. Built with ZenHeart.`
     },
     prism: {
       theme: prismThemes.github,
